7a043b36ec1c4ca606353148e4b9b3d57994c856
Mustafa Salim <salimmustafa763@gmail.com>
2024-02-19 20:28:19 +0300

    #4 menu scroll changed

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="w-full gap-y-2  relative h-[600px] flex flex-col items-center justify-between bg-cover bg-no-repeat bg-center bg-[url('./images/bg-cafe.jpg')]"></header>
+    <header className="w-full gap-y-2  relative h-[400px] lg:h-[600px] flex flex-col items-center justify-between bg-cover bg-no-repeat bg-center bg-[url('./images/bg-cafe.jpg')]"></header>
   )
 }
 
