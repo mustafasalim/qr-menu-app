ed00ed7e58a0fc83e092fddce458a086eaafad0e
Mustafa Salim <salimmustafa763@gmail.com>
2024-02-19 20:26:44 +0300

    #3 vercel error fixed

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,6 +1,3 @@
-import Search from "../../components/search"
-import { GoSearch } from "react-icons/go"
-
 function Header() {
   return (
     <header className="w-full gap-y-2  relative h-[600px] flex flex-col items-center justify-between bg-cover bg-no-repeat bg-center bg-[url('./images/bg-cafe.jpg')]"></header>
