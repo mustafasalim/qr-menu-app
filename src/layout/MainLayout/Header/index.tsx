d6bc31d2ad8d2b6e6a4e7ee0677619c934fc9b96
Mustafa Salim <salimmustafa763@gmail.com>
2024-02-26 03:19:01 +0300

    #error src/layout/MainLayout/index.tsx(5,20): error TS2307: Cannot find module './header' or its corresponding type declarations.

--- src/layout/MainLayout/Header/index.tsx
+++ src/layout/MainLayout/Header/index.tsx
@@ -6,11 +6,12 @@ function Header() {
   const toggleState = useSelector((state: RootState) => state.toggle.value)
   const isToggle = toggleState === true ? "none" : ""
   return (
-    <header className=" w-full lg:h-[400px] h-[300px] flex  items-center justify-center bg-cover bg-no-repeat bg-center bg-[url('https://coffe-app-jade.vercel.app/images/bg-cafe.jpg')]">
+    <header className=" w-full lg:h-[400px] h-[300px] flex items-center justify-center bg-cover bg-no-repeat bg-center bg-[url('https://coffe-app-jade.vercel.app/images/bg-cafe.jpg')]">
       <Snowfall
         style={{ display: `${isToggle}`, position: "relative" }}
         snowflakeCount={50}
       />
+      <hr />
     </header>
   )
 }
